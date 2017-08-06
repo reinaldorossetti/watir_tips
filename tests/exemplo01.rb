3758509c9e5e050b8ce0372d85aea67cbc9b5492
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-08-06 12:40:43 -0300

    Update exemplo01.rb

--- tests/exemplo01.rb
+++ tests/exemplo01.rb
@@ -1,33 +1,36 @@
 require 'watir-webdriver'
 
-x = 0
-y = 1
-
 browser = Watir::Browser.new :ie
 browser.goto "XXX"
 browser.driver.manage.window.maximize
 
+# login
 browser.frame(name:'principal').input(id:'usuario').send_keys 'XXX'
 browser.frame(name:'principal').input(id:'senha').send_keys 'XXX'
 browser.frame(name:'principal').input(id:'button').click
-sleep 7
 
-tests = browser.frame(name:'principal').elements(:tag_name => "div")
-puts tests
-tests.each do |div|
-  if div.value == "Cadastro"
-    x = div.element.wd.location[0]
-    y = div.element.wd.location[1]
-    break
-  end
+def click_submenu
+    x = 0
+    y = 1
+    tests = browser.frame(name:'principal').elements(:tag_name => "div")
+    tests.each do |div|
+      if div.value == "Cadastro"
+        x = div.element.wd.location[0]
+        y = div.element.wd.location[1]
+        break
+      end
+    end
+
+  browser.execute_script("document.elementFromPoint('"+x.to_s+"', '"+y.to_s+"').click()")
+    tests2 = browser.frame(name:'principal').elements(:tag_name => "div")
+    tests2.each do |div|
+      if div.value == "Pesquisar"
+        div.click
+      end
+    end
 end
-puts x,y
 
-browser.execute_script("document.elementFromPoint('"+x.to_s+"', '"+y.to_s+"').click()")
-sleep 3
-tests2 = browser.frame(name:'principal').elements(:tag_name => "div")
-tests2.each do |div|
-  if div.value == "Pesquisar"
-    div.click
-  end
+result = browser.frame(name:'principal').elements(:tag_name => "div").exists?
+if result
+    click_submenu # click in menu javascript.
 end
